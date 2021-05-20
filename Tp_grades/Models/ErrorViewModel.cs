f89755952fe82db3e07efdfa69c8a5dcfa65319e
Yair Valbuena <yair.valbuena@gmail.com>
2021-05-20 14:04:30 -0500

    Agregar archivos de proyecto.

---
+++ Tp_grades/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tp_grades.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
